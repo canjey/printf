f9897be762ab457992d273c51010563520e46797
Carol <carolnjeri32235@gmail.com>
2022-04-22 22:45:20 +0300

    one

--- main.h
+++ main.h
@@ -19,9 +19,6 @@ typedef struct print
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_c(va_list c);
-int print_s(va_list s);
+
 
 #endif
